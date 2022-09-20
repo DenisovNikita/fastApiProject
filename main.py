c12151ff00004ada4a2122d12dc482838107acd9
Nikita Denisov <nikita160412@yandex.ru>
2022-09-20 12:37:11 +0300

    Add endpoint with query parameter

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
