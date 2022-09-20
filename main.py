f060a7616daf0c295b16606c9ef886d5b522d891
Nikita Denisov <nikita160412@yandex.ru>
2022-09-20 12:43:58 +0300

    Add endpoint with request body

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 
 app = FastAPI()
 
@@ -16,6 +26,11 @@ async def say_hello(name: str):
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/items/")
+@app.get("/get_items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip + limit]
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
