11d83d3924a982667d9419cf235c62e04247d0c6
Nikita Denisov <nikita160412@yandex.ru>
2022-09-27 13:51:36 +0300

    Fix comments

--- main.py
+++ main.py
@@ -4,15 +4,8 @@
 from typing import Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-
+from entities import Item
 
 app = FastAPI()
 
