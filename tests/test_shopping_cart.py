8ec8acc1b6c62c3a8c06e7b2975bbc1a30055dd6
Nikita Denisov <nikita160412@yandex.ru>
2022-09-29 18:01:31 +0300

    Add tests

---
+++ tests/test_shopping_cart.py
@@ -0,0 +1,22 @@
+# some_file.py
+import sys
+
+# caution: path[0] is reserved for script path (or '' in REPL)
+sys.path.append('../src')
+
+import pytest
+
+from shopping_cart import ShoppingCart
+
+import pytest
+
+@pytest.fixture
+def shopping_cart_getter():
+    shopping_cart = ShoppingCart()
+    goods = []
+    [shopping_cart.add(good) for good in goods]
+    return shopping_cart
+
+def test_find(shopping_cart_getter):
+    print(*shopping_cart_getter)
+    assert False
