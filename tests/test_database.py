b22c05452866f24b5c69b35dfad7d968a550e948
Nikita Denisov <nikita160412@yandex.ru>
2022-09-29 18:03:31 +0300

    Add tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,6 +1,8 @@
 # some_file.py
 import sys
 
+from contracts import Good
+
 # caution: path[0] is reserved for script path (or '' in REPL)
 sys.path.append('../src')
 
@@ -20,3 +22,9 @@ def database_getter():
 def test_find(database_getter):
     print(*database_getter)
     assert False
+
+def test_add(database_getter):
+    good = Good()
+    database = database_getter
+    database.add(good)
+    assert database.find(good)
